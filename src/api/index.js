69aee198469d91498f53b0cd5119355366d25900
Peter Phenow <peter.phenow@gmail.com>
2021-03-09 00:11:04 -0600

    console logging necessary data

---
+++ src/api/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async () => {
+  try {
+    //destructure the data from res. Can just use data instead of res.data
+    // then further destructure everything inside data
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate }
+    } = await axios.get(url);
+
+    // before all the destructuring, these would be res.data.confirmed, etc.
+    // const modifiedData = {
+    //   confirmed: confirmed,
+    //   recovered: recovered,
+    //   deaths: deaths,
+    //   lastUpdate: lastUpdate
+    // };
+
+    // since the key and value are both the same above, we can remove the value to simplify the object.
+    // const modifiedData = {
+    //   confirmed,
+    //   recovered,
+    //   deaths,
+    //   lastUpdate
+    // };
+
+    //return the data.
+    // return modifiedData;
+
+    //return the data without storing in a variable
+    return {
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate
+    };
+  } catch (err) {}
+};
